f4eecf7f45526ccfb19e79d93a093cd6627e6ea1
Shuji Koike <shuji.koike@me.com>
2020-03-26 20:50:21 +0900

    fixup! [ci skip] WIP WIP WIP WIP WIP

--- src/DemoList.tsx
+++ src/DemoList.tsx
@@ -1,53 +1,72 @@
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import Adjust from "@material-ui/icons/Adjust";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import TreeItem, { TreeItemProps } from "@material-ui/lab/TreeItem";
+import TreeView from "@material-ui/lab/TreeView";
 import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export const DemoList: React.FC = () => {
-  const [state, setState] = React.useState([]);
+  const [state, setState] = React.useState<string[]>([]);
   React.useEffect(() => {
     axios.get("/api/files").then(({ data }) => setState(data));
   }, []);
   return (
-    <ul>
+    <TreeView
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpandIcon={<ChevronRightIcon />}>
       {state.map(e => (
-        <li key={e}>
-          <DemoItem file={e}></DemoItem>
-        </li>
+        <DemoItem key={e} nodeId={e} file={e}></DemoItem>
       ))}
-    </ul>
+    </TreeView>
   );
 };
 
-const DemoItem: React.FC<{ file: string }> = ({ file }) => {
+const DemoItem: React.FC<{
+  file: string;
+} & TreeItemProps> = ({ file, nodeId }) => {
   if (/\.dem$/.test(file)) {
-    return <Link to={`/dem/${file}`}>{file}</Link>;
+    return (
+      <TreeItem
+        nodeId={nodeId}
+        icon={<Adjust />}
+        label={<Link to={`/dem/${file}`}>{file}</Link>}></TreeItem>
+    );
   } else if (/\.rar$/.test(file)) {
     return <RarItem file={file} />;
-  } else {
-    return <span>{file}</span>;
   }
+  return <TreeItem nodeId={nodeId} label={file}></TreeItem>;
 };
 
-const RarItem: React.FC<{ file: string }> = ({ file }) => {
+const RarItem: React.FC<{
+  file: string;
+}> = ({ file }) => {
   const [state, setState] = React.useState([]);
   const [open, setOpen] = React.useState(false);
   React.useEffect(() => {
-    console.log(file);
-    if (!open) return;
-    axios.get(`/api/files/${file}`).then(({ data }) => setState(data));
+    open && axios.get(`/api/files/${file}`).then(({ data }) => setState(data));
   }, [open]);
   return (
     <>
-      <span onClick={() => setOpen(!open)}>{file}</span>
-      {open && (
-        <ul>
-          {state.map((e, i) => (
-            <li>
-              <Link key={i} to={`/dem/${file}?file=${e}`} children={e} />
-            </li>
-          ))}
-        </ul>
-      )}
+      <TreeItem nodeId={file} label={file} onClick={() => setOpen(!open)}>
+        {open ? (
+          state.map((e, i) => (
+            <TreeItem
+              key={e}
+              nodeId={e}
+              icon={<Adjust />}
+              label={
+                <Link key={i} to={`/dem/${file}?file=${e}`}>
+                  {e}
+                </Link>
+              }
+            />
+          ))
+        ) : (
+          <></>
+        )}
+      </TreeItem>
     </>
   );
 };
