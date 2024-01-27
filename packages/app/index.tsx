6cdf8dc161e00e96b1f982ea4e2756dbdd9e717e
Shuji Koike <shuji.koike@me.com>
2024-01-28 04:18:33 +0900

    devops: setup sentry

--- packages/app/index.tsx
+++ packages/app/index.tsx
@@ -2,14 +2,21 @@ import "./firebase"
 import "./index.css"
 
 import * as Sentry from "@sentry/react"
-import { Integrations } from "@sentry/tracing"
 import React from "react"
 import { createRoot } from "react-dom/client"
 
 Sentry.init({
-  dsn: "https://d6b8415d99f44c6b8820ce57e3d742c7@o576396.ingest.sentry.io/6159893",
-  integrations: [new Integrations.BrowserTracing()],
+  dsn: "https://ba991785f82ca8f5667270da5a454902@o1274316.ingest.sentry.io/4506644827144192",
+  integrations: [
+    new Sentry.BrowserTracing(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
   tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
   environment: import.meta.env["MODE"],
 })
 
