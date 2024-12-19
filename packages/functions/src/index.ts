102e2f0209bee4f1cae43aa757b940108b498b1a
Shuji Koike <shuji.koike@me.com>
2024-12-20 03:13:42 +0900

    Update index.ts

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { config, logger, region } from "firebase-functions"
+import { config, logger, region } from "firebase-functions/v1"
 
 const { https } = region("asia-northeast1")
 
